4ac172ce931c319ad528cdad1d1c0c2a963b289a
tkerns1965 <tony.kerns.65@gmail.com>
2017-08-26 06:19:05 -0400

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -1,3 +1,10 @@
+# explicitly create a new volume
+# if omitted, volume will be implicitly created by the 'docker run' statement below
 docker volume create tkdw_webapp_socket
 
-docker run --mount src=tkdw_webapp_socket,dst=/app/socket debian:jessie-slim chown -R www-data:www-data app
+# connect a temporary container to the new volume and change ownership
+docker run --mount src=tkdw_webapp_socket,dst=/app/socket debian:jessie-slim --name tmp-chown-skt \
+  sh -c "chown -R www-data:www-data app"
+
+# remove the temporary container
+docker rm tmp-chown-skt
