1744375c0b9740cfca67a6526f6880c8cec8093d
tkerns1965 <tony.kerns.65@gmail.com>
2017-08-27 09:19:04 -0400

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -1,7 +1,14 @@
-# explicitly create a new volume
-# if omitted, volume will be implicitly created by the 'docker run' statement below
+# explicitly create new volume(s)
+# if omitted, volume(s) will be implicitly created by the 'docker run' statement below
+docker volume create tkdw_db_socket
 docker volume create tkdw_webapp_socket
 
-# connect a temporary container to the new volume and change ownership
-docker run --mount src=tkdw_webapp_socket,dst=/app/socket --rm busybox:1.27.2 \
-  sh -c "chown -R www-data:www-data app"
+# connect a busybox container to the new volume(s) and change ownership
+docker run --rm \
+  --mount src=tkdw_db_socket,dst=/db/socket \
+  --mount src=tkdw_webapp_socket,dst=/app/socket \
+  busybox:1.27.2 \
+  sh -c " \
+    chown -R 999:999 db && \
+    chown -R www-data:www-data app\
+    "
