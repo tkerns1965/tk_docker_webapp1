e730725d8a5051fb12841509041031c9f8812099
tkerns1965 <tony.kerns.65@gmail.com>
2017-08-29 14:43:17 -0400

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -1,12 +1,14 @@
 # explicitly create new volume(s)
 # if omitted, volume(s) will be implicitly created
 #   by the 'docker run' statement below
+docker volume create tkdw_db_data
 docker volume create tkdw_db_socket
 docker volume create tkdw_webapp_socket
 
 # connect a busybox container to the volume(s)
 docker run -di \
   --name bb_temp \
+  --mount src=tkdw_db_data,dst=/db/data \
   --mount src=tkdw_db_socket,dst=/db/socket \
   --mount src=tkdw_webapp_socket,dst=/app/socket \
   busybox:1.27.2
