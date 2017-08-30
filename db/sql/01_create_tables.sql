c63f042d69cdaa96f635a3fdc046ce9b85eeeab3
tkerns1965 <tony.kerns.65@gmail.com>
2017-08-30 10:55:54 -0400

    Update 01_create_tables.sql

--- db/sql/01_create_tables.sql
+++ db/sql/01_create_tables.sql
@@ -21,33 +21,37 @@ CREATE TABLE job (
     company_no  TINYINT      NOT NULL,
     job_id      INT          NOT NULL  AUTO_INCREMENT,
     job_code    VARCHAR(10)  NOT NULL,
-    PRIMARY KEY (job_id)
+    PRIMARY KEY (job_id),
+    UNIQUE KEY (company_no,job_code)
 );
 
 CREATE TABLE timesheet (
     job_id          INT          NOT NULL,
-    timesheet_id    INT          NOT NULL  AUTO_INCREMENT,
+    timesheet_id    INT          NOT NULL  AUTO_INCREMENT=-2147483648,
     timesheet_date  DATE         NOT NULL,
     craft_code      VARCHAR(10)  NOT NULL,
     weather         VARCHAR(50),
     temperature     VARCHAR(20),
-    PRIMARY KEY (timesheet_id)
+    PRIMARY KEY (timesheet_id),
+    UNIQUE KEY (job_id,timesheet_date,craft_code)
 );
 
 CREATE TABLE phase (
     timesheet_id  INT           NOT NULL,
-    phase_id      INT           NOT NULL  AUTO_INCREMENT,
+    phase_id      INT           NOT NULL  AUTO_INCREMENT=-2147483648,
     phase_code    VARCHAR(20)   NOT NULL,
     phase_note    VARCHAR(100),
-    PRIMARY KEY (phase_id)
+    PRIMARY KEY (phase_id),
+    UNIQUE KEY (timesheet_id,phase_code,phase_note)
 );
 
 CREATE TABLE employee (
     timesheet_id   INT           NOT NULL,
-    employee_id    INT           NOT NULL  AUTO_INCREMENT,
+    employee_id    INT           NOT NULL  AUTO_INCREMENT=-2147483648,
     employee_code  VARCHAR(11)   NOT NULL,
     employee_note  VARCHAR(100),
-    PRIMARY KEY (employee_id)
+    PRIMARY KEY (employee_id),
+    UNIQUE KEY (timesheet_id,employee_code,employee_note)
 );
 
 CREATE TABLE class (
@@ -57,7 +61,8 @@ CREATE TABLE class (
     class_note      VARCHAR(100),
     equipment_code  VARCHAR(10),
     equipment_note  VARCHAR(100),
-    PRIMARY KEY (class_id)
+    PRIMARY KEY (class_id),
+    UNIQUE KEY (employee_id,class_code,class_note,equipment_code,equipment_note)
 );
 
 CREATE TABLE hours (
