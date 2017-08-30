13f48206524fd879d4dceaf3bf4f86d241ec3b23
tkerns1965 <tony.kerns.65@gmail.com>
2017-08-30 06:13:28 -0400

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -1,16 +1,16 @@
 # explicitly create new volume(s)
 # if omitted, volume(s) will be implicitly created
 #   by the 'docker run' statement below
-docker volume create tkdw_db_data
-docker volume create tkdw_db_socket
-docker volume create tkdw_webapp_socket
+docker volume create vol_tkdw1_db_data
+docker volume create vol_tkdw1_db_socket
+docker volume create vol_tkdw1_webapp_socket
 
 # connect a busybox container to the volume(s)
 docker run -di \
   --name bb_temp \
-  --mount src=tkdw_db_data,dst=/db/data \
-  --mount src=tkdw_db_socket,dst=/db/socket \
-  --mount src=tkdw_webapp_socket,dst=/app/socket \
+  --mount src=vol_tkdw1_db_data,dst=/db/data \
+  --mount src=vol_tkdw1_db_socket,dst=/db/socket \
+  --mount src=vol_tkdw1_webapp_socket,dst=/app/socket \
   busybox:1.27.2
 
 # change ownership of the folder(s) on the volume(s)
