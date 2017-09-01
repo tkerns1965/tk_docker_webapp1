66fdaca213c43eb206714ce04d8d6cafbdef6bb5
tkerns1965 <tony.kerns.65@gmail.com>
2017-09-01 10:42:22 -0400

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,16 @@
+import datetime
+
+from app import db
+from flask_peewee.auth import BaseUser
+from peewee import *
+
+class User(db.Model, BaseUser):
+    username = CharField()
+    password = CharField()
+    email = CharField()
+    join_date = DateTimeField(default=datetime.datetime.now)
+    active = BooleanField(default=True)
+    admin = BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.username
