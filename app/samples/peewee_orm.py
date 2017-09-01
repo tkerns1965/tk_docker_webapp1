51a847b57fdb7d1b133e957aba109682060ebf31
tkerns1965 <tony.kerns.65@gmail.com>
2017-09-01 09:48:55 -0400

    Update peewee_orm.py

--- app/samples/peewee_orm.py
+++ app/samples/peewee_orm.py
@@ -1,1 +1,9 @@
+from peewee import *
 
+db = MySQLDatabase(user='tony', password='2hard4u', database='tkdw1', unix_socket=/db/socket/mysqld.sock)
+
+class Company(Model):
+    company_no = CharField()
+
+    class Meta:
+        database = db
